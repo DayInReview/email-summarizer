689246f342200cc004c1616a75c17b30cf57592d
ishan0102 <ishan0102@gmail.com>
2020-07-27 15:18:58 -0500

    Switch from Flask to Electron framework

--- DayInReview/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '02c4e75b494eafafb778726c93c11bf5'
-
-from DayInReview import routes
--- DayInReview/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
-
-class DisplayForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Submit')
