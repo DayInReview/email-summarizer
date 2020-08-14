8e128d52695d1636dc58b33ca19fdf037c7422f1
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-08-14 14:21:23 -0500

    Packet being sent is of the correct format and includes all relevant information

--- DayInReview/js/home.js
+++ DayInReview/js/home.js
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 const ipcRenderer = require('electron').ipcRenderer;
 const nunjucks = require('nunjucks');
 nunjucks.configure({ autoescape: true });
@@ -15,24 +15,13 @@ ipcRenderer.on('loginInfo', (event, arg) => {
 // runs when user logs in
 function getSummaries(email, password) {
     localStorage.clear();
-    
-    const summary = spawn('python', ['app.py', '-e', email, '-p', password]);
 
-    summary.on('close', (code) => {
-        console.log(`child process exited with code: ${code}`);
-        if (code == 1) {
-            window.location.href = "login.html";
-        }
-    });
-
-    summary.stdout.on('data', (data) => {
-        var parsed = data.toString(); // grab the email_summaries from the python script
-
+    exec(`python app.py -e ${email} -p ${password}`, (error, stdout, stderr) => {
         // puts content in correct JSON/HTML format
-        parsed = parsed.replace(/</g, "&lt"); // escape all left angle brackets
+        parsed = stdout.replace(/</g, "&lt"); // escape all left angle brackets
         parsed = parsed.replace(/>/g, "&gt"); // escape all right angle brackets
-        parsed = parsed.replace(/'/g, "\""); // replace all the single quotes with double quotes
         parsed = parsed.replace(/\\n/g, " "); // replace all the newlines with a space
+        console.log(stdout);
         
         parsed = JSON.parse(parsed); // convert stdout to JSON object
         parsed = JSON.stringify(parsed); // convert JSON object to string
@@ -41,11 +30,13 @@ function getSummaries(email, password) {
         window.location.href = "home.html";
         localStorage.setItem('emails', parsed); // store JSON string in local storage
         loadHomePage();
+    }).on('exit', (code) => {
+        console.log(`child process exited with code: ${code}`);
     });
 }
 
 function loadHomePage() {
-    var all_emails = JSON.parse(localStorage.getItem('emails'));
+    var all_emails = Object.values(JSON.parse(localStorage.getItem('emails')));
     console.log(all_emails);
     console.log(all_emails.length);
     document.getElementById("from").innerHTML = all_emails[0]['from'];
